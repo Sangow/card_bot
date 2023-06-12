89dd9322e70651a182f7d6137e2aeddfe356238c
sangow <sergey.anpilogich@gmail.com>
2023-06-12 15:04:33 +0300

    added delete sql method

--- sq.py
+++ sq.py
@@ -11,15 +11,20 @@ async def start() -> None:
     db.commit()
 
 
-async def get_cards_nicknames(user_id: int) -> list:
-    return cur.execute('SELECT card_nickname FROM cards WHERE user_id = (?)', (user_id,)).fetchall()
+async def get_cards_nicknames(user_id: str) -> list:
+    return cur.execute('SELECT card_nickname FROM cards WHERE user_id=(?)', (user_id,)).fetchall()
 
 
-async def get_card_number(user_id: int, card_nickname: str) -> str:
-    return cur.execute('SELECT card_number FROM cards WHERE user_id = (?) AND card_nickname = (?)',
+async def get_card_number(user_id: str, card_nickname: str) -> str:
+    return cur.execute('SELECT card_number FROM cards WHERE user_id=(?) AND card_nickname=(?)',
                        (user_id, card_nickname)).fetchone()[0]
 
 
-async def add_card(user_id: int, card_number: str, card_nickname: str) -> None:
+async def add_card(user_id: str, card_number: str, card_nickname: str) -> None:
     cur.execute('INSERT INTO cards VALUES (?, ?, ?)', (user_id, card_number, card_nickname))
+    db.commit()
+
+
+async def delete_card(user_id: str, card_number: str) -> None:
+    cur.execute('DELETE FROM cards WHERE user_id=(?) and card_number=(?)', (user_id, card_number))
     db.commit()
