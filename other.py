98accd5403927bde16688237f5d75f9d29a4801b
sangow <sergey.anpilogich@gmail.com>
2023-07-05 17:32:25 +0300

    added str.isdigit() verification

--- other.py
+++ other.py
@@ -1,3 +1,5 @@
 def luhn_check_card(card_number: str) -> bool:
-    r = [int(ch) for ch in str(card_number)][::-1]
-    return (sum(r[0::2]) + sum(sum(divmod(d * 2, 10)) for d in r[1::2])) % 10 == 0
+    if card_number.isdigit():
+        r = [int(ch) for ch in str(card_number)][::-1]
+        return (sum(r[0::2]) + sum(sum(divmod(d * 2, 10)) for d in r[1::2])) % 10 == 0
+    return False
