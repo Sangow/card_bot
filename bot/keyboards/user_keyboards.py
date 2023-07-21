389fda468fa4ea571512fec7e3e8c521a873a59a
sangow <sergey.anpilogich@gmail.com>
2023-07-21 17:39:01 +0300

    smth changed

--- bot/keyboards/user_keyboards.py
+++ bot/keyboards/user_keyboards.py
@@ -1,14 +1,18 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
-
 start_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='SHOW CARDS'), KeyboardButton(text='ADD CARD')]],
                                resize_keyboard=True)
 
 cancel_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='CANCEL')]],
-                                resize_keyboard=True)
+                                resize_keyboard=True, )
 
 confirm_kb = ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text='CONFIRM'), KeyboardButton(text='CANCEL')]],
                                  resize_keyboard=True)
+
+delete_edit_inline_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='✏️',
+                                                                                    callback_data='edit_callback'),
+                                                               InlineKeyboardButton(text='❌',
+                                                                                    callback_data='delete_callback')]])
 
 
 def nickname_kb(card_nicknames: list) -> ReplyKeyboardMarkup:
@@ -18,9 +22,3 @@ def nickname_kb(card_nicknames: list) -> ReplyKeyboardMarkup:
         rkm.insert(c_n[0])
 
     return rkm
-
-
-delete_inline_kb = InlineKeyboardMarkup(inline_keyboard=[[InlineKeyboardButton(text='✏️',
-                                                                               callback_data='edit_callback'),
-                                                          InlineKeyboardButton(text='❌',
-                                                                               callback_data='delete_callback')]])
