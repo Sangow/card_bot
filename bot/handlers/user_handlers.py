38086c7b2374fb763d7ebf9977e5744261ec7ff8
sangow <sergey.anpilogich@gmail.com>
2023-07-21 17:38:51 +0300

    smth changed

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -1,9 +1,10 @@
 from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, CallbackQuery
 
-from bot.keyboards import start_kb, cancel_kb, confirm_kb, nickname_kb, delete_inline_kb
+from bot.keyboards import start_kb, cancel_kb, confirm_kb, nickname_kb, delete_edit_inline_kb
 from bot.sql import get_cards_nicknames, add_card, get_card_number, delete_card
 from bot.states import AddCard, ShowCard
+from .service import validate_card_number
 
 
 async def cmd_start(message: Message) -> None:
@@ -31,6 +32,9 @@ async def add_card_1(message: Message) -> None:
 
 async def add_card_2(message: Message, state: FSMContext) -> None:
     async with state.proxy() as data:
+        if not validate_card_number(message.text):
+            await message.answer(text='Credit card is invalid.')
+            return
         data['card_number'] = message.text
 
     await message.answer(text='Enter card nickname:')
@@ -81,7 +85,7 @@ async def show_cards_nicknames(message: Message) -> None:
 async def show_card_number(message: Message, state: FSMContext) -> None:
     await message.answer(text=f'`{await get_card_number(user_id=message.from_user.id, card_nickname=message.text)}`',
                          parse_mode='markdown',
-                         reply_markup=delete_inline_kb)
+                         reply_markup=delete_edit_inline_kb)
 
     await message.answer(text='Choose button:',
                          reply_markup=start_kb)
