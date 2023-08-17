e2b41397c599517f8f65a90a3a722c0d8ee31cda
sangow <sergey.anpilogich@gmail.com>
2023-08-17 14:56:39 +0300

    [dev] added editing handlers

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -5,11 +5,16 @@ from bot.states import AddCard, ShowCard
 from .service import validate_card_number, validate_card_nickname
 from .user_handlers import cmd_start, cmd_cancel, \
     add_card_1, add_card_2, add_card_2_fail, add_card_3, add_card_3_fail, \
-    add_card_4, show_cards_nicknames, show_card_number
+    add_card_4, show_nickname_list, show_card_number, edit_card_handler, \
+    delete_card_handler, edit_card_number_previous_handler, \
+    edit_card_number_fail_handler, edit_card_number_handler, \
+    confirm_edited_card_handler
 
 
 async def register_handlers(dp: Dispatcher) -> None:
-    dp.register_message_handler(cmd_start, CommandStart(), state='*')
+    dp.register_message_handler(cmd_start,
+                                CommandStart(),
+                                state='*')
     dp.register_message_handler(cmd_cancel,
                                 Text(equals='cancel', ignore_case=True),
                                 state='*')
@@ -32,7 +37,41 @@ async def register_handlers(dp: Dispatcher) -> None:
                                 Text(equals='confirm', ignore_case=True),
                                 state=AddCard.confirm)
 
-    dp.register_message_handler(show_cards_nicknames,
+    dp.register_message_handler(show_nickname_list,
                                 Text(equals='show cards', ignore_case=True))
     dp.register_message_handler(show_card_number,
                                 state=ShowCard.show_card_number)
+
+    dp.register_message_handler(edit_card_handler,
+                                Text(equals='✏️'),
+                                state=ShowCard.edit_delete_card)
+
+    dp.register_message_handler(edit_card_number_previous_handler,
+                                Text(equals='leave previous',
+                                     ignore_case=True),
+                                state=ShowCard.edit_card_number)
+    dp.register_message_handler(edit_card_number_fail_handler,
+                                lambda message:
+                                not validate_card_number(message.text),
+                                state=ShowCard.edit_card_number)
+    dp.register_message_handler(edit_card_number_handler,
+                                state=ShowCard.edit_card_number)
+
+    # dp.register_message_handler(edit_card_nickname_previous_handler,
+    #                             Text(equals='leave previous',
+    #                                  ignore_case=True),
+    #                             state=ShowCard.edit_card_nickname)
+    # dp.register_message_handler(edit_card_nickname_fail_handler,
+    #                             lambda message:
+    #                             not validate_card_nickname(message.text),
+    #                             state=ShowCard.edit_card_nickname)
+    # dp.register_message_handler(edit_card_nickname_handler,
+    #                             state=ShowCard.edit_card_nickname)
+
+    dp.register_message_handler(confirm_edited_card_handler,
+                                Text(equals='confirm', ignore_case=True),
+                                state=ShowCard.confirm)
+
+    dp.register_message_handler(delete_card_handler,
+                                Text(equals='❌'),
+                                state=ShowCard.edit_delete_card)
